74fb3204e777f9d065c33aeec32c555e5509b9f2
Eldor [FE16] <118098131+eldoresanov@users.noreply.github.com>
2022-11-22 18:36:25 +0500

    Add files via upload

---
+++ uygavazifa-js/scripts.js
@@ -0,0 +1,11 @@
+let MyCard = {
+	Name: "Eldor",
+	Surname: "Esanov",
+	Age: 13,
+	FrontEnd:"FE[16]",
+    EducationCenter:"Open Web Academy"
+}
+console.log(MyCard)
+let bir = prompt('birinchi sonni kiriting')
+let ikki = prompt('ikkinchisonni kiriting')
+console.log(Number(bir) % Number(ikki)) 
